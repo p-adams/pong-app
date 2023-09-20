b3f47d0be96373acce57dd3acd332044fc066223
Paul Adams <ghusiyy@gmail.com>
2023-09-20 00:32:58 -0700

    draw left and right paddles

--- src/lib/PongGame.svelte
+++ src/lib/PongGame.svelte
@@ -1,14 +1,55 @@
 <script lang="ts">
-  import { onMount } from "svelte";
+  import { afterUpdate, onMount } from "svelte";
   const [width, height] = [600, 350];
+  const [paddleWidth, paddleHeight] = [25, 75];
+
   let canvas: HTMLCanvasElement;
   let ctx: CanvasRenderingContext2D;
 
   onMount(() => {
     ctx = canvas.getContext("2d")!;
-    ctx.fillStyle = "green";
-    ctx.fillRect(0, 0, width, height);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    leftPaddle.draw();
+    rightPaddle.draw();
   });
+
+  afterUpdate(() => {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    leftPaddle.draw();
+    rightPaddle.draw();
+  });
+
+  $: leftPaddleX = 0;
+  $: leftPaddleY = (height - paddleHeight) / 2;
+  $: rightPaddleX = width - paddleWidth;
+  $: rightPaddleY = (height - paddleHeight) / 2;
+
+  $: leftPaddle = {
+    x: leftPaddleX,
+    y: leftPaddleY,
+    draw: () => {
+      ctx.save();
+      ctx.beginPath();
+      ctx.rect(leftPaddle.x, leftPaddle.y, paddleWidth, paddleHeight);
+      ctx.fillStyle = "white";
+      ctx.fill();
+      ctx.closePath();
+      ctx.restore();
+    },
+  };
+  $: rightPaddle = {
+    x: rightPaddleX,
+    y: rightPaddleY,
+    draw: () => {
+      ctx.save();
+      ctx.beginPath();
+      ctx.rect(rightPaddle.x, leftPaddle.y, paddleWidth, paddleHeight);
+      ctx.fillStyle = "white";
+      ctx.fill();
+      ctx.closePath();
+      ctx.restore();
+    },
+  };
 </script>
 
 <canvas {width} {height} bind:this={canvas} />
@@ -16,5 +57,6 @@
 <style>
   canvas {
     outline: 1px solid lightgray;
+    background-color: #444;
   }
 </style>
