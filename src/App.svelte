e1a8a6797f05b1c95da2912b3df4152438de02e1
Paul Adams <ghusiyy@gmail.com>
2023-09-19 23:00:34 -0700

    init pong component

--- src/App.svelte
+++ src/App.svelte
@@ -1,31 +1,23 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import PongGame from "./lib/PongGame.svelte";
 </script>
 
 <main>
   <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+    <h1>Pong</h1>
+
+    <div class="card">
+      <PongGame />
+    </div>
+
+    <p>
+      How to Play <a
+        href="https://github.com/sveltejs/kit#readme"
+        target="_blank"
+        rel="noreferrer">Guide</a
+      >
+    </p>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
---
+++ src/lib/PongGame.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+</script>
+
+<canvas width="600" height="350" />
+
+<style>
+  canvas {
+    outline: 1px solid lightgray;
+  }
+</style>
