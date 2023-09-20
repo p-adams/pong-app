a613ae503dda5c63443bdb7d4241e5b285184e17
Paul Adams <ghusiyy@gmail.com>
2023-09-19 23:29:13 -0700

    set up canvas context

--- src/lib/PongGame.svelte
+++ src/lib/PongGame.svelte
@@ -1,7 +1,17 @@
 <script lang="ts">
+  import { onMount } from "svelte";
+  const [width, height] = [600, 350];
+  let canvas: HTMLCanvasElement;
+  let ctx: CanvasRenderingContext2D;
+
+  onMount(() => {
+    ctx = canvas.getContext("2d")!;
+    ctx.fillStyle = "green";
+    ctx.fillRect(0, 0, width, height);
+  });
 </script>
 
-<canvas width="600" height="350" />
+<canvas {width} {height} bind:this={canvas} />
 
 <style>
   canvas {
