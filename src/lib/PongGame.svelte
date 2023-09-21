deae2765842d0f7b365a061594af53f849aba78c
Paul Adams <ghusiyy@gmail.com>
2023-09-20 23:25:09 -0700

    draw net

--- src/lib/PongGame.svelte
+++ src/lib/PongGame.svelte
@@ -2,22 +2,10 @@
   import { afterUpdate, onMount } from "svelte";
   const [width, height] = [600, 350];
   const [paddleWidth, paddleHeight] = [25, 75];
+  const [netWidth, netHeight] = [25, 350];
 
   let canvas: HTMLCanvasElement;
   let ctx: CanvasRenderingContext2D;
-
-  onMount(() => {
-    ctx = canvas.getContext("2d")!;
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    leftPaddle.draw();
-    rightPaddle.draw();
-  });
-
-  afterUpdate(() => {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    leftPaddle.draw();
-    rightPaddle.draw();
-  });
 
   $: leftPaddleX = 0;
   $: leftPaddleY = (height - paddleHeight) / 2;
@@ -50,6 +38,37 @@
       ctx.restore();
     },
   };
+
+  $: net = {
+    x: width / 2,
+    y: height,
+    draw: () => {
+      ctx.setLineDash([10, 5]);
+      ctx.strokeStyle = "white";
+      ctx.lineWidth = 4;
+      ctx.beginPath();
+      ctx.moveTo(net.x, 1);
+      ctx.lineTo(net.x, net.y);
+      ctx.stroke();
+      ctx.closePath();
+    },
+  };
+
+  function renderGame() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    net.draw();
+    leftPaddle.draw();
+    rightPaddle.draw();
+  }
+
+  onMount(() => {
+    ctx = canvas.getContext("2d")!;
+    renderGame();
+  });
+
+  afterUpdate(() => {
+    renderGame();
+  });
 </script>
 
 <canvas {width} {height} bind:this={canvas} />
